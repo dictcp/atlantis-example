903a096bef892b84f6f5ec051f32051ecf220628
Dick Tang <dick.tang@ymail.com>
2019-05-13 17:19:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,14 @@
 resource "null_resource" "example" {
 }
+
+data "terraform_remote_state" "foo" {
+  backend = "remote"
+
+  config = {
+    organization = "hk01"
+
+    workspaces {
+      name = "test workspace"
+    }
+  }
+}
